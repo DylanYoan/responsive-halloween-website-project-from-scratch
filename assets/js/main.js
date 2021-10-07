fe62f468ac798e1da42d0fee072e0d35a92ab2e1
DylanYoan <dylanyoanmc@gmail.com>
2021-10-07 13:04:51 -0500

    Home and buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,7 +27,14 @@ function linkAction() {
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== HOME SWIPER ===============*/
-
+var homeSwiper = new Swiper(".home-swiper", {
+    spaceBetween: 30,
+    loop: 'true',
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    },
+});
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 
